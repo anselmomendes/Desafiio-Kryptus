20b68b08a99e67aa3011c0ca0d88b7acc1b968a2
Anselmo <anselmo.96@hotmail.com>
2020-11-13 11:41:05 -0300

    Manutenção das funções

--- Questão 01..c
+++ Questão 01..c
@@ -12,34 +12,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-#Anselmo Mendes Oliveira
-
-#tipo de dado
+//tipo de dado
 typedef struct lista Lista;
 
-#Enserir um item na lista
+//Enserir um item na lista
 void put();
 
-#Recuperar um item da lista
+//Recuperar um item da lista
 Lista* get(Lista** l);
 
-#Listar todos os items da lista
+//Listar todos os items da lista
 void list(Lista* l);
 
-#Remover item da lista
-void remove(Lista* l);
+//Remover item da lista
+void remove_lista(Lista* l);
 
-#Esvaziar lista
+//Esvaziar lista
 void clear(Lista* l);
 
-#Recuperar o primeiro item da lista
+//Recuperar o primeiro item da lista
 void first(Lista* l);
 
-#Recuperar o ultimo item da lista;
+//Recuperar o ultimo item da lista;
 void last(Lista* l);
 
-void print_entry(char *entry) {
-	printf("You entered: %s\n", entry);
+
+
+
+int main(int argc, char *argv[]) {
+
+struct lista {
+	int info;
+	struct lista* prox;
+};
+
+Lista* a;
+
+
+	return EXIT_SUCCESS;
 }
 
 void put(Lista** l, int i) {
@@ -60,14 +70,4 @@ void list(Lista* l) {
 	for(aux = l; aux != NULL; aux = aux->prox) {
 		printf("%d ", aux->info);
 	}
-}
-
-int main(int argc, char *argv[]) {
-
-struct lista {
-	int info;
-	struct lista* prox;
-};
-
-	return EXIT_SUCCESS;
 }
